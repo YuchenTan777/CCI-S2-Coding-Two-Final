c8ae9f58e5ead66f7a3095230749c9805be9bb85
Yuchen Tan <73331906+YuchenTan777@users.noreply.github.com>
2021-04-30 14:10:57 +0100

    Create Build the Topic Model.py

---
+++ step by step/Build the Topic Model.py
@@ -0,0 +1,19 @@
+# Create Dictionary
+id2word = corpora.Dictionary(data_ready)
+
+# Create Corpus: Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in data_ready]
+
+# Build LDA model
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                           id2word=id2word,
+                                           num_topics=4, 
+                                           random_state=100,
+                                           update_every=1,
+                                           chunksize=10,
+                                           passes=10,
+                                           alpha='symmetric',
+                                           iterations=100,
+                                           per_word_topics=True)
+
+pprint(lda_model.print_topics())
