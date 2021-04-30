34d8e8d676681eae50ad0b28b9353193f8a957a1
Yuchen Tan <73331906+YuchenTan777@users.noreply.github.com>
2021-04-30 14:10:34 +0100

    Create Build the Bigram, Trigram Models and Lemmatize.py

---
+++ step by step/Build the Bigram, Trigram Models and Lemmatize.py
@@ -0,0 +1,22 @@
+# Build the bigram and trigram models
+bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100) # higher threshold fewer phrases.
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100)  
+bigram_mod = gensim.models.phrases.Phraser(bigram)
+trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+# !python3 -m spacy download en  # run in terminal once
+def process_words(texts, stop_words=stop_words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]    
+    return texts_out
+
+data_ready = process_words(data_words)  # processed Text Data!
