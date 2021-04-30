04c28103003942eecc1459ff22f06632fbb7be87
Yuchen Tan <73331906+YuchenTan777@users.noreply.github.com>
2021-04-30 14:10:08 +0100

    Create Tokenize Sentences and Clean.py

---
+++ step by step/Tokenize Sentences and Clean.py
@@ -0,0 +1,12 @@
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True) 
+        yield(sent)  
+
+# Convert to list
+data = df.content.values.tolist()
+data_words = list(sent_to_words(data))
+print(data_words[:1])
